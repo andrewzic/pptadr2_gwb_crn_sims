9f53f1b4338b133f949153af4e2eef9f4bf97953
Andrew Zic <andrew.john.zic@gmail.com>
2021-05-26 14:18:32 +1000

    first commit

---
+++ ptasim_26psr_common_similar/slurm/slurm_DP5_ppta_pe_interporf_fixed_gamma_20_nfreq_be_jup_el_20210510.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=ppta_common_similar_mc_spin_v_spincommon
+#SBATCH --output=/flush5/zic006/pptadr2_gwb_crn_sims/slurm_logs/ppta_common_similar_mc_spin_v_spincommon_20210510_%A_%a.log
+#SBATCH --ntasks=4
+#SBATCH --time=1-21:30
+#SBATCH --mem-per-cpu=7G
+#SBATCH --tmp=8G
+##SBATCH --array=0-25
+
+# pyv="$(python -c 'import sys; print(sys.version_info[0])')"
+# if [ "$pyv" == 2 ]
+# then
+#     echo "$pyv"
+#     module load numpy/1.16.3-python-2.7.14
+# fi
+
+
+module load singularity
+
+singularity exec /home/zic006/psr_gwb.sif which python3
+singularity exec /home/zic006/psr_gwb.sif echo $TEMPO2
+singularity exec /home/zic006/psr_gwb.sif echo $TEMPO2_CLOCK_DIR
+
+singularity exec /home/zic006/psr_gwb.sif python3 /flush5/zic006/pptadr2_gwb_crn_sims/run_analysis.py --prfile "/flush5/zic006/pptadr2_gwb_crn_sims/params/params_DP5_ppta_pe_interporf_fixed_gamma_20nfreq_be_jup_el_20210510.dat"
