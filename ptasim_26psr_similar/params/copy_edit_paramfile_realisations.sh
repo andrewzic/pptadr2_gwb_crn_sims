f73cba67670f0b6b78acd717292f2d1070ae9958
Andrew <andrew.john.zic@gmail.com>
2021-05-28 10:15:53 +1000

    fixed typo in paramfile dirs

--- ptasim_26psr_similar/params/copy_edit_paramfile_realisations.sh
+++ ptasim_26psr_similar/params/copy_edit_paramfile_realisations.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-for i in `seq 2 9`; do echo $i ; cp params_all_mc_array_spin_v_spincommon_20210524_r0.dat params_all_mc_array_spin_v_spincommon_20210524_r${i}.dat; sed -i 's|real_0|real_'${i}'|g' params_all_mc_array_spin_v_spincommon_20210524_r${i}.dat; sed -i 's|20210524_0|20210524_'${i}'|g' params_all_mc_array_spin_v_spincommon_20210524_r${i}.dat; done
+for i in `seq 2 9`; 
+do 
+    echo $i ; 
+    cp params_all_mc_array_spin_v_spincommon_20210524_r0.dat params_all_mc_array_spin_v_spincommon_20210524_r${i}.dat; 
+    sed -i 's|real_0|real_'${i}'|g' params_all_mc_array_spin_v_spincommon_20210524_r${i}.dat; 
+    sed -i 's|20210524_0|20210524_'${i}'|g' params_all_mc_array_spin_v_spincommon_20210524_r${i}.dat;
+done
+
+for i in params_all_mc_array_spin_v_spincommon_20210524_r*.dat;
+do 
+    echo $i;
+    sed -i 's|zic006/ssb/ptasim/|zic006/pptadr2_gwb_crn_sims/|g' $i;
+done
